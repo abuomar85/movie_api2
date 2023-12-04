62a4fae1fe2e732fba303cf41c1cd3bfbb1e6f87
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2023-12-04 12:03:54 +0100

    update index.js

--- index.js
+++ index.js
@@ -20,11 +20,20 @@ const express = require('express'),
       app.use(bodyParser.json());
 
       //mongoose.connect('mongodb://localhost:27017/moviesDB2', {useNewUrlParser: true, useUnifiedTopology: true});
-   
-
-      mongoose.connect(process.env.CONNECTION_URI,
+      mongoose.connect('mongodb+srv://abuomaralyabrode85:Abu.0796422917@cluster0.usnlqqf.mongodb.net/?retryWrites=true&w=majority',
       {useNewUrlParser: true, useUnifiedTopology: true}
       );
+
+/*       mongoose.connect(process.env.CONNECTION_URI,
+      {useNewUrlParser: true, useUnifiedTopology: true}
+      ); */
+
+/* mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Error connecting to MongoDB:', err)); */
 
 //the main page of app
 app.get('/' ,(req, res) => {
